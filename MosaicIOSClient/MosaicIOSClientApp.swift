b2761b0bed80c4a2d6e5c66a0e426ab0ad37a71c
José Henrique Fernandes Silva <henrique@MacBook-Pro-de-Jose.local>
2024-07-03 22:41:20 -0300

    Cria o app iOS

---
+++ MosaicIOSClient/MosaicIOSClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MosaicIOSClientApp.swift
+//  MosaicIOSClient
+//
+//  Created by José Henrique Fernandes Silva on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MosaicIOSClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
